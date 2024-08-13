e8ef935f96a40c98585581672d6de71e29a2e732
dejan <dejan.pelemis@cyberlab.rs>
2024-08-13 14:07:25 +0200

    Updated models to adjust database

--- model/Tour.go
+++ model/Tour.go
@@ -5,17 +5,16 @@ import (
 )
 
 type Tour struct {
-	Id          int       `json: "id"`
-	Info        *TourInfo `json: "info"`
-	AuthorId    int       `json: "author_id"`
-	PublishTime time.Time `json: "publish_time"`
-	Equipments  []int64   `json: "equipments"`
-	Checkpoints []int64   `json: "checkpoints"`
-	Objects     []int64   `json: "objects"`
-	//TourReviews []*TourReview
-	FootTime    float64  `json: "foot_time"`
-	BicycleTime float64  `json: "bicycle_time"`
-	CarTime     float64  `json: "car_time"`
-	TotalLength float64  `json: "total_length"`
-	Images      []string `json: "images"`
+	Id          int       `json:"id"`          // Fixed JSON tag syntax
+	Info        *TourInfo `json:"info"`        // Fixed JSON tag syntax
+	AuthorId    int       `json:"author_id"`    // Fixed JSON tag syntax
+	PublishTime time.Time `json:"publish_time"` // Fixed JSON tag syntax
+	Equipments  []int64   `json:"equipments"`  // Fixed JSON tag syntax
+	Checkpoints []int64   `json:"checkpoints"` // Fixed JSON tag syntax
+	Objects     []int64   `json:"objects"`     // Fixed JSON tag syntax
+	FootTime    float64   `json:"foot_time"`   // Fixed JSON tag syntax
+	BicycleTime float64   `json:"bicycle_time"`// Fixed JSON tag syntax
+	CarTime     float64   `json:"car_time"`    // Fixed JSON tag syntax
+	TotalLength float64   `json:"total_length"`// Fixed JSON tag syntax
+	Images      []string `json:"images"`      // Fixed JSON tag syntax
 }
